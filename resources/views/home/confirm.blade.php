eb1b57f5f0b10caaea7e6a243eea95ce8e53026d
julie <2020007485@student.sit.ac.nz>
2021-10-03 12:44:33 +1300

    Add content to the Confirmation notification page

--- resources/views/home/confirm.blade.php
+++ resources/views/home/confirm.blade.php
@@ -11,26 +11,52 @@
 
 <!-- Featured Starts Here -->
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-                <h2>Thank you for booking with Mobile Health.</h2>
-
-            <p>You have booked for:</p>
-            <p>Name</p>
-            <p>Date</p>
-            <p>Course</p>
-
-            <p>The course will be held at xxxxxxxxxxxxx and will commence at 9am.</p>
-
-            <p>If you have any questions, please feel free to contact us.</p>
-
-            <p><a href="/home/terms" id="is_terms_agreed" target="_blank">Terms and conditions</a> are available.</p>
+    <div class="section-heading">
+        <h4>Thank you for booking with Mobile Health.</h4>
+        <div class="row">
+            <div class="col-md-12">
+                <p>You have booked for:</p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <p>Name</p>
+            </div>
+            <div class="col-md-8 col-sm-12">
+                <p>xxxxxxxxxxxxxx</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <p>Course</p>
+            </div>
+            <div class="col-md-8 col-sm-12">
+                <p>xxxxxxxxxxxxxx</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <p>Date and Start Time</p>
+            </div>
+            <div class="col-md-8 col-sm-12">
+                <p>xxxxxxxxxxxxxx, xxxxx am</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <p>Location</p>
+            </div>
+            <div class="col-md-8 col-sm-12">
+                <p>xxxxxxxxxxxxxx</p>
             </div>
         </div>
 
-        </div>
+
+        <p>You will receive an email confirmation of your booking and payment shortly.</p>
+
+        <p>If you have any questions, please feel free to contact us.</p>
+
+        <p><a href="/home/terms" id="is_terms_agreed" target="_blank">Please review our Terms and Conditions for
+                information about the course.</a></p>
     </div>
-</div>
 </div>
 
 
