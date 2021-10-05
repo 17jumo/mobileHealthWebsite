2dca81085b0da1c8a54a35575a563d30d03aee50
julie <2020007485@student.sit.ac.nz>
2021-10-05 13:41:47 +1300

    password reset formatting

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,3 +1,63 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container">
+        <div class="card card-body card-login w-75 align-items-left">
+{{--            <img src="/images/MobileHealthTabLogo.png"
+                 alt="Mobile Health Logo">--}}
+            <h4>Reset Password</h4>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div class="row">
+                    <p>Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+
+                    <!-- Email Address -->
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="email">Email*</label>
+                            <input name="email" type="text" class="form-control" {{--placeholder="Email *" --}}value="{{ __('Email') }}" autofocus />
+                        </div>
+                    </div>
+{{--
+                    <!-- Password -->
+                    <div class="col-md-12">
+                        <div class="form-group content-start">
+                            <label for="password">Password*</label>
+                            <input name="password" type="password" class="form-control" placeholder="Password"
+                                   value=""/>
+                        </div>
+                    </div>--}}
+                </div>
+                <br>
+{{--                <x-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-button>--}}
+{{--                <input class="btn btn-primary" type="submit" value="Login">--}}
+                <a class="btn btn-primary" href="/">{{ __('Email Password Reset Link') }}</a>
+{{--                <a class="btn btn-outline-dark" href="{{ route('password.request') }}">Reset Password </a>--}}
+            </form>
+
+            <!-- Validation Errors -->
+            @if ($errors->any())
+                <div class="alert alert-danger mt-2">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </div>
+
+@endsection
+
+
+{{--
+
+
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -34,3 +94,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+--}}
