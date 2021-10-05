c53ba1ab28d8409eba50248865ae1a3bfdaa582e
julie <2020007485@student.sit.ac.nz>
2021-10-05 13:19:56 +1300

    Gate security implementation and display fields in table

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -17,8 +17,8 @@
                 <table class="table table-striped" id="book-table">
                     <thead>
                     <tr>
-                        <th scope="col" onclick="sortTable(0)">ID</th>
-                        <th scope="col" onclick="sortTable(1)">Firstname</th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Firstname</th>
                         <th scope="col">Lastname</th>
                         <th scope="col">Email</th>
                         <th scope="col">Phone Number</th>
@@ -26,7 +26,6 @@
                         <th scope="col">Course Name</th>
                         <th scope="col">Course Date</th>
                         <th scope="col">Course Total</th>
-                        <th scope="col">coursedate id - tempfield</th>
                         <th scope="col">Actions</th>
                     </tr>
                     </thead>
@@ -39,10 +38,9 @@
                             <td>{{$booking->email}}</td>
                             <td>{{$booking->phone}}</td>
                             <td>{{$booking->company_name}}</td>
-                            <td>{{$booking->course_name}} </td>
+                            <td>{{$booking->coursedate->course->course_name}}</td>
                             <td>@php echo ($booking->coursedate->scheduled_date)->format('d-m-Y') @endphp</td>
                             <td>${{$booking->course_total}} </td>
-                            <td>{{$booking->coursedate->id}}</td>
                             <td>
                                 <form action="/bookings/{{$booking->id}}" method="POST">
                                     @method('DELETE')
@@ -51,9 +49,11 @@
                                         <a class="btn btn-outline-primary mx-1 " href="/bookings/{{$booking->id}}">Show</a>
                                         <a class="btn btn-outline-success mx-1"
                                            href="/bookings/{{$booking->id}}/edit">Edit</a>
-                                        <button type="submit" title="delete" class="btn btn-outline-secondary mx-1">
-                                            Delete
-                                        </button>
+                                        @can('isAdmin')
+                                            <button type="submit" title="delete" class="btn btn-outline-secondary mx-1">
+                                                Delete
+                                            </button>
+                                        @endcan
                                     @endauth
                                 </form>
                             </td>
@@ -66,60 +66,4 @@
         </div>
     </div>
 
-        <script>
-            function sortTable(n) {
-                var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-                table = document.getElementById("book-table");
-                switching = true;
-                // Set the sorting direction to ascending:
-                dir = "asc";
-                /* Make a loop that will continue until
-                no switching has been done: */
-                while (switching) {
-                    // Start by saying: no switching is done:
-                    switching = false;
-                    rows = table.rows;
-                    /* Loop through all table rows (except the
-                    first, which contains table headers): */
-                    for (i = 1; i < (rows.length - 1); i++) {
-                        // Start by saying there should be no switching:
-                        shouldSwitch = false;
-                        /* Get the two elements you want to compare,
-                        one from current row and one from the next: */
-                        x = rows[i].getElementsByTagName("TD")[n];
-                        y = rows[i + 1].getElementsByTagName("TD")[n];
-                        /* Check if the two rows should switch place,
-                        based on the direction, asc or desc: */
-                        if (dir == "asc") {
-                            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                                // If so, mark as a switch and break the loop:
-                                shouldSwitch = true;
-                                break;
-                            }
-                        } else if (dir == "desc") {
-                            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                                // If so, mark as a switch and break the loop:
-                                shouldSwitch = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (shouldSwitch) {
-                        /* If a switch has been marked, make the switch
-                        and mark that a switch has been done: */
-                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                        switching = true;
-                        // Each time a switch is done, increase this count by 1:
-                        switchcount ++;
-                    } else {
-                        /* If no switching has been done AND the direction is "asc",
-                        set the direction to "desc" and run the while loop again. */
-                        if (switchcount == 0 && dir == "asc") {
-                            dir = "desc";
-                            switching = true;
-                        }
-                    }
-                }
-            }
-        </script>
 @endsection
