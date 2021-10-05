ec0f8912e1936f3b8e81edf08aec03366930937a
julie <2020007485@student.sit.ac.nz>
2021-10-05 14:02:23 +1300

    New layout

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -38,3 +38,32 @@ home.show.blade
             </div>
         </div>
     </div>
+
+
+<div class="row">
+    @foreach($courses as $course)
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+
+            <div class="card card-body card-index">
+                <img src="/images/heart.png"
+                     alt="picture of course being held">
+                <h5>{{$course->course_name}}</h5>
+                <h6>Duration: {{$course->duration}}
+                    @if($course->duration > 1)
+                        days
+                    @else
+                        day
+                    @endif
+                </h6>
+                <h6>Cost: ${{$course->price}}</h6>
+                <p>{{$course->course_desc_short}}</p>
+                <p><a href="/courses/1"><sub id="readmore">Read more</sub></a></p>
+
+                <a href="/bookings/create" class="btn btn-info">Book now</a>
+
+            </div>
+        </div>
+    @endforeach
+</div>
+</div>
+
