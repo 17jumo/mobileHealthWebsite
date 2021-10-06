3ee53fe36bba28770ffe8d5f978dbdc78161a82b
‘Tom <woodward.t27@gmail.com>
2021-10-06 13:34:20 +1300

    table commented out in Course Dates/Show

--- resources/views/coursedates/show.blade.php
+++ resources/views/coursedates/show.blade.php
@@ -33,45 +33,45 @@
                             </div>
                 @endforeach
 
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th scope="col">Field</th>
-                            <th scope="col">Entry</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <td scope="row">{{$coursedate->id}}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Course ID</th>
-                            <td scope="row">{{$coursedate->course_id}}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Course Name</th>
-                            <td scope="row">course_name - yet to show</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Course Date</th>
-                            <th scope="row">{{$coursedate->scheduled_date->format('d-m-Y')}}</th>
-                        </tr>
-                        <tr>
-                            <th scope="col">Attendee Maximum</th>
-                            <th scope="row">{{$coursedate->max_attendee}}</th>
-                        </tr>
-                        <tr>
-                            <th scope="col">Venue</th>
-                            <td scope="row">{{$coursedate->venue}}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                @auth
-                    <a class="btn btn-primary" href="/coursedates/{{$coursedate->id}}/edit">Edit</a>
-                @endauth
+{{--                <div class="table-responsive">--}}
+{{--                    <table class="table table-striped">--}}
+{{--                        <thead>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Field</th>--}}
+{{--                            <th scope="col">Entry</th>--}}
+{{--                        </tr>--}}
+{{--                        </thead>--}}
+{{--                        <tbody>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">ID</th>--}}
+{{--                            <td scope="row">{{$coursedate->id}}</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Course ID</th>--}}
+{{--                            <td scope="row">{{$coursedate->course_id}}</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Course Name</th>--}}
+{{--                            <td scope="row">course_name - yet to show</td>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Course Date</th>--}}
+{{--                            <th scope="row">{{$coursedate->scheduled_date->format('d-m-Y')}}</th>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Attendee Maximum</th>--}}
+{{--                            <th scope="row">{{$coursedate->max_attendee}}</th>--}}
+{{--                        </tr>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Venue</th>--}}
+{{--                            <td scope="row">{{$coursedate->venue}}</td>--}}
+{{--                        </tr>--}}
+{{--                        </tbody>--}}
+{{--                    </table>--}}
+{{--                </div>--}}
+{{--                @auth--}}
+{{--                    <a class="btn btn-primary" href="/coursedates/{{$coursedate->id}}/edit">Edit</a>--}}
+{{--                @endauth--}}
                 <a class="btn btn-warning mx-1" href="/coursedates/">Cancel</a>
             </div>
         </div>
