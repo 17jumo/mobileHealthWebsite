ec6dd61d2cbac6ce4595c52fd73f5d5b75be332f
julie <2020007485@student.sit.ac.nz>
2021-10-07 23:27:24 +1300

    Update layout for show page

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -13,18 +13,15 @@
 
             <div class="row">
                 <div class="col-md-12">
-                    <div class="section-heading">
                         <h1>First Aid Courses Available</h1>
                         <hr>
-                    </div>
                     @foreach($courses as $course)
-                        <div class="card w-75 mt-2 card-login">
-                            <div class="card-header">
-                                <img src="/images/index/heart.png" alt="Course Icon">
-                            </div>
+                        <div class="card w-100 mt-2 card-login">
                             <div class="card-body">
+                                <img src="/images/index/heart.png" alt="Course Icon">
                                 <h5 class="card-title">{{$course->course_name}}</h5>
-                                <h6 class="card-title">${{$course->price}}</h6>
+                                <br><br>
+                                <h6 class="card-title">${{$course->price}} inc GST</h6>
                                 <br><br>
                                 <p class="card-text">{{$course->course_desc_long}}</p>
                                 <br><br>
@@ -49,8 +46,8 @@
                                 {{--                @endif--}}
                             </div>
                         </div>
+                    @endforeach
                 </div>
             </div>
-        @endforeach
 
 @endsection
