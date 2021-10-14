ef74ee884a4d53a619beb2d0e9064f22e616cf82
julie <2020007485@student.sit.ac.nz>
2021-10-15 11:34:26 +1300

    Code tidy - remove commented lines

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,10 +10,6 @@ class Booking extends Model
     use HasFactory;
     protected $guarded = [];
 
-/*    protected $casts = [
-        'dob' => 'date'
-    ];*/
-
     public function coursedate() {
         return $this->belongsTo(Coursedate::class);
     }
