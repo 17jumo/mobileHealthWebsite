3426c95043e5c3ce9492f47f521b45c472d4879c
julie <2020007485@student.sit.ac.nz>
2021-10-15 13:39:51 +1300

    update seeder to only create user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(1)->create(['email'=> 'a@a.com', 'isAdmin' => true]);
         \App\Models\User::factory(1)->create(['email'=> 'test@test.com']);
         \App\Models\User::factory(5)->create();
-        \App\Models\Course::factory(4)->create();
+/*        \App\Models\Course::factory(4)->create();
         \App\Models\Coursedate::factory(10)->create();
-        \App\Models\Booking::factory(5)->create();
+        \App\Models\Booking::factory(5)->create();*/
 
     }
 }
