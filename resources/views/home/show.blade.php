d565c4b8bde9b9dbff63ab1af9b6e7d8ddb25389
julie <2020007485@student.sit.ac.nz>
2021-10-15 20:21:19 +1300

    Page will now only show active courses

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -13,16 +13,15 @@
                     <h1>First Aid Courses Available</h1>
                     <hr>
                     @foreach($courses as $course)
-                        <div class="card w-100 mt-2 card-login">
-                            @if($course->isActive == 1)
+                        @if($course->isActive == 1)
+                            <div class="card w-100 mt-2 card-login">
                                 <div class="card-body">
-
-                                    <img src="/images/index/{{$course->image}}" alt="Course Icon">
-                                    <h5 class="card-title">{{$course->course_name}}</h5>
-                                    <h6 class="card-title">${{$course->price}} inc GST</h6>
-                                    <p class="card-text">{{$course->course_desc_long}}</p>
+                                    <img src="/images/index/{{ $course->image }}" alt="Course Icon">
+                                    <h5 class="card-title">{{ $course->course_name }}</h5>
+                                    <h6 class="card-title">${{ $course->price }} inc GST</h6>
+                                    <p class="card-text">{{ $course->course_desc_long }}</p>
                                     <br><br>
-                                    <p class="card-text">Duration: &nbsp{{$course->duration}}
+                                    <p class="card-text">Duration: &nbsp{{ $course->duration }}
                                         @if($course->duration > 1)
                                             days
                                         @else
@@ -30,16 +29,14 @@
                                         @endif</p>
                                     <p class="card-text">
                                         Starts at: &nbsp{{ date('G:i', strtotime($course->start_time)) }}
-                                        &nbsp&nbsp&nbsp&nbsp
                                     </p>
                                     <p class="card-text">
                                         Ends at: &nbsp{{ date('G:i', strtotime($course->end_time)) }}</p>
 
                                     <a class="btn btn-book" href="/bookings/create">Book now</a>
-
                                 </div>
-                            @endif
-                        </div>
+                            </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
