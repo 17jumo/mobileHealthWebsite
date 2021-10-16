671da59b4dafb66ed181d8afd7cbebf8f97a6ee9
julie <2020007485@student.sit.ac.nz>
2021-10-16 19:21:41 +1300

    Code tidy alignment

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Booking details</h2>
-        </div>
-        @if($booking->isActive == 0)
-            <div class="alert alert-danger" role="alert">Inactive Record</div>
-        @endif
-    </div>
+    <h2>Booking details</h2>
+
+    @if($booking->isActive == 0)
+        <div class="alert alert-danger" role="alert">Inactive Record</div>
+    @endif
+
     <div class="table-responsive">
         <table class="table table-striped">
             <thead>
@@ -100,88 +98,6 @@
         <a class="btn btn-primary" href="/bookings/{{$booking->id}}/edit">Edit</a>
     @endauth
     <a class="btn btn-warning mx-1" href="/bookings/">Cancel</a>
-    </div>
-    </div>
-    </div>
-    </div>
+
 
 @endsection
-{{--
-
-
-@extends('layouts.app')
-@section('content')
-
-    <div class="container">
-
-        <a class="btn btn-primary mx-1" href="/courses/show">View and Amend Courses</a>
-        <a class="btn btn-primary mx-1 " href="/coursedates/show">View and Amend Course Dates</a>
-        <a class="btn btn-primary mx-1 " href="/bookings/show">View and Amend Booking</a>
-
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-heading">
-                    <h2>booking - SHOW</h2>
-                    <hr>
-                </div>
-
-
-    <div id="showView" class="form-body">
-        <div class="row">
-            <div class="form-holder">
-                <div class="form-content">
-                    <div class="form-items">
-
-                        @auth
-                            <div class="buttonContainer">
-                                <a class="btn btn-primary" href="/bookings/{{$booking->id}}/edit">Edit Booking</a>
-                            </div>
-                        @endauth
-
-                        <h3>{{$booking->firstname}}</h3>
-                        <p>{{$booking->lastname}}</p>
-
-
-                        @method('PUT')
-                        @csrf
-
-                        <div id="activityContainer">
-
-                            <h3>{{$booking->firstname}} {{$booking->lastname}}</h3>
-                            <p>Email {{$booking->email}}, DOB {{$booking->dob}}, Gender {{$booking->gender}} Mobile: {{$booking->mobile}}
-                                CompName: {{$booking->companyName}} Street: {{$booking->addressStreet}}
-                                Suburb: {{$booking->addressSuburb}} City: {{$booking->addressCity}} PostCode: {{$booking->addressPostcode}}
-                                Country: {{$booking->addressCountry}}
-                                CourseDate: {{$booking->courseDate}}</p>
-
-                            <div>
-                                <p>Distance</p>
-                                <h2>{{$booking->email}}</h2>
-                            </div>
-                            <div>
-                                <p>Total Minutes</p>
-                                <h2>{{$booking->dob}}</h2>
-                            </div>
-                            <div>
-                                <p>Date</p>
-                                <h2>{{$booking->gender}}</h2>
-                            </div>
-                            <div>
-                                <p>Sport</p>
-                                <h2>{{$booking->mobile}}</h2>
-                            </div>
-                        </div>
-
-
-
-
-                        <br>
-                        <a class="btn btn-warning mx-1" href="/bookings/">Cancel</a>
-                        <br><br>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection--}}
