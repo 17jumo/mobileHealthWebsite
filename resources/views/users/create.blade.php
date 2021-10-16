df061a2c96070fd943cbdd4fbb06445d25a79302
julie <2020007485@student.sit.ac.nz>
2021-10-16 19:08:22 +1300

    Code tidy alignment

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,42 +1,53 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-md-12">
-                <h2>Add New User</h2>
-                <hr>
+    <h2>Add New User</h2>
+    <hr>
 
-            <div class="form-content">
-                <form method="POST" action="/users">
-                    @csrf
+    <div class="form-content">
+        <form method="POST" action="/users">
+            @csrf
 
-                    <div class="col-md-6">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                               name="name" value="{{ @old('name') }}" required>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="email">Email</label>
-                        <input type="text" class="form-control @error('email') is-invalid @enderror"
-                               id="email" name="email" value="{{ @old('email') }}" required>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                               id="password" name="password" value="">
-                    </div>
-                    <div class="col-md-6">
-                        <label for="isAdmin">Administrator (0=No, 1=Yes)</label>
-                        <input type="text" class="form-control @error('isAdmin') is-invalid @enderror"
-                               id="isAdmin" name="isAdmin" value="{{ @old('isAdmin') }}">
-                    </div>
-                    <div class="form-button mt-3">
-                        <input class="btn btn-primary" type="submit" value="Save">
-                        <a class="btn btn-warning mx-1" href="/users">Cancel</a>
-                    </div>
-                </form>
+            <div class="form-group col-md-6">
+                <label for="name">Name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                       name="name" value="{{ @old('name') }}" required>
             </div>
-        </div>
+
+            <div class="form-group col-md-6">
+                <label for="email">Email</label>
+                <input type="text" class="form-control @error('email') is-invalid @enderror"
+                       id="email" name="email" value="{{ @old('email') }}" required>
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="password">Password</label>
+                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                       id="password" name="password" value="">
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="isAdmin">Administrator (0=No, 1=Yes)</label>
+                <input type="text" class="form-control @error('isAdmin') is-invalid @enderror"
+                       id="isAdmin" name="isAdmin" value="{{ @old('isAdmin') }}">
+            </div>
+
+            <div class="form-button mt-3">
+                <input class="btn btn-primary" type="submit" value="Save">
+                <a class="btn btn-warning mx-1" href="/users">Cancel</a>
+            </div>
+        </form>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
     </div>
 
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,71 +1,60 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="section-heading">
-                <h2>Users</h2>
-                <hr>
-            </div>
+    <h2>Users</h2>
+    <hr>
 
-            <p>
-                @can('isAdmin')
-                    <a class="btn btn-outline-primary mx-1 "
-                       href="/users/create">Register new user</a>
-                @endcan
-            </p>
+    <p>
+        @can('isAdmin')
+            <a class="btn btn-outline-primary mx-1 "
+               href="/users/create">Register new user</a>
+        @endcan
+    </p>
 
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Administrator</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($users as $user)
-                        <tr>
-                            <th scope="row">{{$user->id}}</th>
-                            <th>{{$user->name}}</th>
-                            <td>{{$user->email}}</td>
-                            <td>
-                                @if($user->isAdmin == 1)
-                                    Yes
+    <table class="table table-striped table-responsive">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Administrator</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{$user->id}}</th>
+                <th>{{$user->name}}</th>
+                <td>{{$user->email}}</td>
+                <td>
+                    @if($user->isAdmin == 1)
+                        Yes
+                    @else
+                        No
+                    @endif
+                </td>
+                <td>
+                    <form action="/users/{{$user->id}}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        @auth
+                            <a class="btn btn-outline-success mx-1"
+                               href="/users/{{$user->id}}/edit">Edit</a>
+                            @can('isAdmin')
+                                @if(Auth::user() == $user)
                                 @else
-                                    No
+                                    <button type="submit" title="delete" class="btn btn-outline-secondary mx-1">
+                                        Delete</button>
                                 @endif
-                            </td>
-                            <td>
-                                <form action="/users/{{$user->id}}" method="POST">
-                                    @method('DELETE')
-                                    @csrf
-                                    @auth
-                                        <a class="btn btn-outline-success mx-1"
-                                           href="/users/{{$user->id}}/edit">Edit</a>
-                                        @can('isAdmin')
-                                            @if(Auth::user() == $user)
-                                            @else
-                                                <button type="submit" title="delete"
-                                                        class="btn btn-outline-secondary mx-1">
-                                                    Delete
-                                                </button>
-                                            @endif
-                                        @endcan
-                                    @endauth
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-
+                            @endcan
+                        @endauth
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 
 @endsection
 
